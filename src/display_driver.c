76376eea4351ba6a561f26a1cd5f113f7191b431
victor <victorkrook96@gmail.com>
2021-05-04 18:36:24 +0200

    Made a simple display driver

---
+++ src/display_driver.c
@@ -0,0 +1,56 @@
+#include "display_driver.h"
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <fcntl.h>
+
+
+struct colorStruct** front_buffer;
+struct colorStruct** back_buffer;
+
+const char* driverFilePath = "/home/victor/coding/projects/ziozir/gui/display_driver";
+const size_t TOTAL_PIXELS = SCREEN_HEIGHT * SCREEN_WIDTH;
+const size_t BUFF_SIZE = TOTAL_PIXELS * sizeof(struct colorStruct);
+
+
+static inline void ensure_driver_file_path_exists()
+{
+    FILE* fd = fopen(driverFilePath, "r");
+    if (fd) {
+        fclose(fd);
+    } else {
+        printf("Failed to open display driver file at: \"%s\"\n", driverFilePath);
+        printf("Exiting...\n");
+        exit(0);
+    }
+}
+
+void display_init()
+{
+    front_buffer = (struct colorStruct**) malloc(BUFF_SIZE);
+    back_buffer = (struct colorStruct**) malloc(BUFF_SIZE);
+    memset(front_buffer, 0, BUFF_SIZE);
+    memset(back_buffer, 0, BUFF_SIZE);
+    ensure_driver_file_path_exists();
+}
+
+void display_swap_buffers()
+{
+    struct colorStruct** tmp = front_buffer;
+    front_buffer = back_buffer;
+    back_buffer = tmp;
+}
+
+void display_put(const int row, const int col, const struct colorStruct color)
+{
+    front_buffer[row][col] = color;
+}
+
+void display_send_buffer()
+{
+    printf("Sending buffer...\n");
+    FILE* fd = fopen(driverFilePath, "w");
+    size_t written = fwrite(front_buffer, sizeof(struct colorStruct), TOTAL_PIXELS, fd);
+    printf("Written %ld bytes\n", written);
+    fclose(fd);
+}
---
+++ src/display_driver.h
@@ -0,0 +1,28 @@
+#ifndef DISPLAY_DRIVER_H
+#define DISPLAY_DRIVER_H
+
+#ifndef SCREEN_WIDTH
+    #define SCREEN_WIDTH 400
+#endif
+#ifndef SCREEN_HEIGHT
+    #define SCREEN_HEIGHT 400
+#endif
+
+struct colorStruct{
+    unsigned char x;
+};
+
+/* Initializes the display. */
+void display_init();
+
+/* Swaps the front buffer with the back buffer. */
+void display_swap_buffers();
+
+/* Puts a colorStruct at given row and col. */
+void display_put(const int row, const int col, const struct colorStruct);
+
+/* Sends the buffer data to the display. */
+void display_send_buffer();
+
+
+#endif
