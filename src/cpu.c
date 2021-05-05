5354c8c4bc9a1ac091fe9c83e9f85c8b83b10bc5
victor <victorkrook96@gmail.com>
2021-05-05 18:12:35 +0200

    Added alot of new instructions. Mainly boilerplate code

--- src/cpu.c
+++ src/cpu.c
@@ -13,27 +13,22 @@ static word ram[RAM_SIZE];
 static StatusReg sReg;
 
 
-
 /* --- Primary memory --- */
 #define WRITE_RAM(value, addr) {ram[addr] = value;}
 #define READ_RAM(addr) {ram[addr]}
-
 
 void writeRam(address addr, word value)
 {
     ram[addr] = value;
 }
-
 word readRam(address addr)
 {
     return ram[addr];
 }
-
 word readReg(reg reg)
 {
     return ram[REG_OFFSET + reg];
 }
-
 void writeReg(reg reg, word value)
 {
     ram[REG_OFFSET + reg] = value;
@@ -42,8 +37,6 @@ void writeReg(reg reg, word value)
 
 /* --- Secondary memory --- */
 const char* memoryPath = MEMORY_FILE_PATH;
-
-
 word readMemory(memoryAddress addr)
 {
     word buf;
@@ -58,7 +51,6 @@ word readMemory(memoryAddress addr)
     close(fd);
     return buf;
 }
-
 int writeMemory(memoryAddress addr, word value)
 {
     int fd = open(memoryPath, O_WRONLY);
@@ -74,45 +66,39 @@ int writeMemory(memoryAddress addr, word value)
 }
 
 
-
 /* --- Utils --- */
 StatusReg getStatusReg()
 {
     return sReg;
 }
-
 void updateStatusReg()
 {
     sReg.zFlag = READ_SREG_BIT(STATUS_REG_Z_FLAG);
 }
-
 void updateZFlag(word result)
 {
     WRITE_SREG_BIT(result == 0 ? 1 : 0);
 }
-
-
-
 static inline void incPc()
 {
     ram[PC]++;
 }
-
 static inline void setPc(address value)
 {
     ram[PC] = value;
 }
+
 
 /* --- IO --- */
 void cpuPutc(char c)
 {
     putchar(c);
 }
-
 char cpuGetc()
 {
     return getchar();
 }
+
 
 /* --- Instructions --- */
 static inline void arithmetic(word result, address to)
@@ -205,57 +191,82 @@ int run()
     char msg[200];
     int running = 1;
     int instrNbr = 0;
-    Instruction* instructions;
-    Instruction instr = instructions[0];
+    Instruction instr = fetchInstruction();
 
-    switch (instr.op) {
-        case OP_ADD:
-            opAdd(instr.args[0], instr.args[1], instr.args[2]);
-            break;
-        case OP_SUB:
-            opSub(instr.args[0], instr.args[1], instr.args[2]);
-            break;
-        case OP_MUL:
-            opMul(instr.args[0], instr.args[1], instr.args[2]);
-            break;
-        case OP_JUMP:
-            opJump(instr.args[0]);
-            break;
-        case OP_JUMPZ:
-            opJumpZ(instr.args[0]);
-            break;
-        case OP_JUMPEQ:
-            opJumpEq(instr.args[0], instr.args[1], instr.args[2]);
-            break;
-        case OP_JUMPNEQ:
-            opJumpNeq(instr.args[0], instr.args[1], instr.args[2]);
-            break;
-        case OP_JUMPGT:
-            opJumpGT(instr.args[0], instr.args[1], instr.args[2]);
-            break;
-        case OP_JUMPGTE:
-            opJumpGTE(instr.args[0], instr.args[1], instr.args[2]);
-            break;
-        case OP_JUMPLT:
-            opJumpLT(instr.args[0], instr.args[1], instr.args[2]);
-            break;
-        case OP_JUMPLTE:
-            opJumpLTE(instr.args[0], instr.args[1], instr.args[2]);
-            break;
-        case OP_NOP:
-            opNop();
-            break;
-        case OP_HALT:
-            sprintf(msg, "[%d] Halt reached!\n", instrNbr);
-            LOG_INFO(msg);
-            running = 0;
-            break;
-        case OP_UNKNOWN:
-        default:
-            sprintf(msg, "[%d] Unknown operation %d. Exiting!\n", instrNbr, instr.op);
-            LOG_INFO(msg);
-            running = 0;
-            break;
+    while (running) {
+        switch (instr.op) {
+            case OP_ADD:
+                opAdd(instr.args[0], instr.args[1], instr.args[2]);
+                break;
+            case OP_ADDI:
+                opAddI(instr.args[0], instr.args[1], instr.args[2]);
+                break;
+            case OP_SUB:
+                opSub(instr.args[0], instr.args[1], instr.args[2]);
+                break;
+            case OP_SUBI:
+                opSubI(instr.args[0], instr.args[1], instr.args[2]);
+                break;
+            case OP_MUL:
+                opMul(instr.args[0], instr.args[1], instr.args[2]);
+                break;
+            case OP_MULI:
+                opMulI(instr.args[0], instr.args[1], instr.args[2]);
+                break;
+            case OP_AND:
+                opAnd(instr.args[0], instr.args[1], instr.args[2]);
+                break;
+            case OP_ANDI:
+                opAndI(instr.args[0], instr.args[1], instr.args[2]);
+                break;
+            case OP_NOT:
+                opNot(instr.args[0], instr.args[1], instr.args[2]);
+                break;
+            case OP_NOTI:
+                opNotI(instr.args[0], instr.args[1], instr.args[2]);
+                break;
+
+
+            case OP_JUMP:
+                opJump(instr.args[0]);
+                break;
+            case OP_JUMPZ:
+                opJumpZ(instr.args[0]);
+                break;
+            case OP_JUMPEQ:
+                opJumpEq(instr.args[0], instr.args[1], instr.args[2]);
+                break;
+            case OP_JUMPNEQ:
+                opJumpNeq(instr.args[0], instr.args[1], instr.args[2]);
+                break;
+            case OP_JUMPGT:
+                opJumpGT(instr.args[0], instr.args[1], instr.args[2]);
+                break;
+            case OP_JUMPGTE:
+                opJumpGTE(instr.args[0], instr.args[1], instr.args[2]);
+                break;
+            case OP_JUMPLT:
+                opJumpLT(instr.args[0], instr.args[1], instr.args[2]);
+                break;
+            case OP_JUMPLTE:
+                opJumpLTE(instr.args[0], instr.args[1], instr.args[2]);
+                break;
+            case OP_NOP:
+                opNop();
+                break;
+            case OP_HALT:
+                sprintf(msg, "[%d] Halt reached!\n", instrNbr);
+                LOG_INFO(msg);
+                running = 0;
+                break;
+            case OP_UNKNOWN:
+            default:
+                sprintf(msg, "[%d] Unknown operation %d. Exiting!\n", instrNbr, instr.op);
+                LOG_INFO(msg);
+                running = 0;
+                break;
         }
+    }
 
+    printf("CPU Stopped.\n");
 }
--- src/cpu.h
+++ src/cpu.h
@@ -41,7 +41,7 @@ void writeReg(reg reg, word value);
 
 /* --- Secondary memory --- */
 #define MEMORY_SIZE 1024
-#define MEMORY_FILE_PATH "//home/victor/coding/projects/cpu/src/memory"
+#define MEMORY_FILE_PATH "/home/victor/coding/projects/cpu/src/memory"
 typedef uint16_t memoryAddress;
 
 word readMemory(memoryAddress addr);
@@ -50,19 +50,43 @@ int writeMemory(memoryAddress addr, word value);
 
 /* --- Instructions --- */
 typedef enum {
-    OP_ADD,
-    OP_SUB,
-    OP_MUL,
-    OP_JUMP,
-    OP_JUMPZ,
-    OP_JUMPEQ,
-    OP_JUMPNEQ,
-    OP_JUMPGT,
-    OP_JUMPGTE,
-    OP_JUMPLT,
-    OP_JUMPLTE,
-    OP_NOP,
-    OP_HALT,
+    OP_ADD,     // ADD R1 R2
+    OP_ADDI,    // ADD R1 2
+    OP_SUB,     // MUL R1 R2
+    OP_SUBI,    // MUL R1 2
+    OP_MUL,     // MUL R1 R2
+    OP_MULI,    // MUL R1 2
+    OP_AND,     // AND R1 R2
+    OP_ANDI,    // AND R1 1
+    OP_NOT,     // NOT R1 R2
+    OP_NOTI,    // NOT R1 1
+    OP_OR,      // OR R1 R2
+    OP_ORI,     // OR R1 1
+    OP_XOR,     // XOR R1 R2
+    OP_XORI,    // XOR R1 1
+    OP_OP_LD,   // LD R1 R2
+    OP_OP_LDI,  // LD R1 2
+    OP_PUSH,    // PUSH
+    OP_POP,     // POP
+    OP_INC,     // INC R1
+    OP_DEC,     // DEC R1
+    OP_EI,      // Enable Interrupts
+    OP_DI,      // Disable Interrupts
+    OP_SET,     // SET 1 R1  -  Set (b, reg)
+    OP_RES,     // RET 1 R1  -  Reset (b, reg)
+    OP_CALL,
+    OP_RET,
+    OP_RETI,
+    OP_JUMP,    // JMP
+    OP_JUMPZ,   // JMPZ
+    OP_JUMPEQ,  // JMPEQ
+    OP_JUMPNEQ, // JMPNEQ
+    OP_JUMPGT,  // JPMGT
+    OP_JUMPGTE, // JMPGTE
+    OP_JUMPLT,  // JMPLT
+    OP_JUMPLTE, // JMPLTE
+    OP_NOP,     // NOP
+    OP_HALT,    // HALT
     OP_UNKNOWN
 } Operation;
 
@@ -74,9 +98,45 @@ typedef struct {
 } Instruction;
 
 void opNop();
-void opAdd(word op1, word op2, address to);
-void opSub(word op1, word op2, address to);
-void opMul(word op1, word op2, address to);
+
+/* Arithmetic */
+void opAdd(reg r1, reg r2, address to);
+void opAddI(reg r, word op, address to);
+void opSub(reg r1, reg r2, address to);
+void opSubI(reg r, word op, address to);
+void opMul(reg r1, reg r2, address to);
+void opMulI(reg r, word op, address to);
+void opAnd(reg r1, reg r2, address to);
+void opAndI(reg r, word op, address to);
+void opNot(reg r1, reg r2, address to);
+void opNotI(reg r, word op, address to);
+void opOr(reg r1, reg r2, address to);
+void opOrI(reg r, word op, address to);
+void opXor(reg r1, reg r2, address to);
+void opXorI(reg r, word op, address to);
+
+void opLd(reg r1, reg r2, address to);
+void opLdI(reg r, word op, address to);
+
+void opPush(reg r);
+void opPop(reg r);
+void opInc(reg r);
+void opDec(reg r);
+
+/* Enable interrupts. */
+void opEI();
+/* Disable Interrupts. */
+void opDI();
+/* Set bit b, in reg r to 1 */
+void opSet(uint8_t bit, reg r);
+/* Set bit b, in reg r to 0 */
+void opRes(uint8_t bit, reg r);
+
+void opCall(reg r1, reg r2, address to);
+void opRet(reg r, word op, address to);
+void opReti(reg r, word op, address to);
+
+
 void opJump(address addr);
 void opJumpZ(address addr);
 void opJumpEq(word op1, word op2, address addr);
@@ -85,6 +145,8 @@ void opJumpGT(word op1, word op2, address addr);
 void opJumpGTE(word op1, word op2, address addr);
 void opJumpLT(word op1, word op2, address addr);
 void opJumpLTE(word op1, word op2, address addr);
+
+Instruction fetchInstruction();
 
 /* --- IO --- */
 void cpuPutc(char c);
