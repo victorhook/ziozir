9c6598e9dc5833a4893bda7fe00db8e625027d5f
victor <victorkrook96@gmail.com>
2021-05-10 18:19:27 +0200

    Created different output formats

--- assembler/filehandler.py
+++ assembler/filehandler.py
@@ -1,4 +1,10 @@
+import binascii
+import io
+
+
 class FileHandler:
+
+    MAX_HEX_BYTES_PER_ROW = 10
 
     @classmethod
     def read(cls, filepath: str) -> str:
@@ -7,7 +13,40 @@ class FileHandler:
             return f.read()
 
     @classmethod
-    def save(cls, data: bytes, filepath: str, mode: str ='wb') -> None:
+    def _save_as_str(cls, data: bytes, filepath: str,
+                     formatter: callable) -> None:
+        # Group by every byte, seperate by whitespace
+        buf = io.StringIO()
+        i = 0
+        byte_nbr = 0
+        for byte in data:
+            if byte_nbr > cls.MAX_HEX_BYTES_PER_ROW:
+                buf.write('\n')
+                byte_nbr = 0
+
+            buf.write(formatter(byte) + ' ')
+            i += 2
+            byte_nbr += 1
+
+        buf.seek(0)
+        cls._save(buf.read(), filepath, mode='w')
+
+    @classmethod
+    def save_hex(cls, data: bytes, filepath: str) -> None:
+        cls._save_as_str(data, filepath, lambda byte:
+                         hex(byte)[2:].zfill(2).upper())
+
+    @classmethod
+    def save_bin(cls, data: bytes, filepath: str) -> None:
+        cls._save_as_str(data, filepath, lambda byte:
+                         bin(byte)[2:].zfill(8))
+
+    @classmethod
+    def save_exe(cls, data: bytes, filepath: str) -> None:
+        cls._save(data, filepath)
+
+    @classmethod
+    def _save(cls, data: bytes, filepath: str, mode: str = 'wb') -> None:
         """ Saves data to the given filepathy. """
         with open(filepath, mode) as f:
             f.write(data)
